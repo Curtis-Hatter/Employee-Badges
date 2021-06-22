b406f2d3ad272ac4f3f73f8df19181e5aa1985a3
Curtis H <sitrucrettah1@gmail.com>
2021-06-21 22:09:15 -0700

    Init plus Main function plus Util class that Prints employee information to a CVS file while creating badge images for each employee utilizing C# code

---
+++ CatWorx.BadgeMaker/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static List<Employee> GetEmployess()
+        {
+            //I will retrun a List of Strings
+            List<Employee> employees = new List<Employee>();
+            while (true)
+            {
+                Console.WriteLine("Please enter the first name (leave empty to exit): ");
+                string firstName = Console.ReadLine();
+                if (firstName == "")
+                {
+                    break;
+                };
+
+                // Add a Console.ReadLine() for each value
+                Console.WriteLine("Please enter the last name: ");
+                string lastName = Console.ReadLine();
+                Console.WriteLine("Please enter the ID: ");
+                int id = Int32.Parse(Console.ReadLine());
+                Console.WriteLine("Enter Photo URL: ");
+                string photoUrl = Console.ReadLine();
+
+                // Create a new Emplyee instance
+                Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
+                employees.Add(currentEmployee);
+            }
+            return employees;
+        }
+
+        static void Main(string[] args)
+        {
+            // This is our employee-getting code now
+            List<Employee> employees = new List<Employee>();
+
+            employees = GetEmployess();
+
+            Util.MakeCSV(employees);
+            Util.MakeBadges(employees);
+        }
+    }
+}
