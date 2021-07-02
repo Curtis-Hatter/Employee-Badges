c0b90926075e7198de22cb25f0fd0e019c9946dc
Curtis H <sitrucrettah1@gmail.com>
2021-07-01 19:51:18 -0700

    Add projects learned on C#

---
+++ Other Works/NestedIf/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedIf
+{
+    class Program
+    {
+        static int highscore = 38;
+        static string highscorePlayer = "Curtis";
+        static void Main(string[] args)
+        {
+            int playerScore = 200;
+            string playerName = "James";
+            Console.WriteLine("Playing the game!");
+            TheGame(playerScore, playerName);
+            Console.Read();
+
+        }
+        public static void TheGame(int score, string player)
+        {
+            if(score > highscore)
+            {
+                highscore = score;
+                highscorePlayer = player;
+
+                Console.WriteLine("New Highscore is " + score + "!");
+                Console.WriteLine("New Highscore holder is " + player + "!");
+            }
+            else
+            {
+                Console.WriteLine("The old highscore of " + highscore + " could not be broken and is still held by " + highscorePlayer);
+            }
+            /*switch(score)
+            {
+                case  > highscore:
+                    Console.WriteLine("New Highscore is " + score + "!");
+                    Console.WriteLine("New Highscore holder is " + player + "!");
+                        break;
+                default:
+                    Console.WriteLine("The old highscore of " + highscore + " could not be broken and is still held by " + highscorePlayer);
+                        break;
+                    
+            }*/
+        }
+    }
+}
